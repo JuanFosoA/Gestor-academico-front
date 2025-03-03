f813ebd6156926d2fd468091fd5b80a4b150a5f6
YioJoe <yioyiomain@gmail.com>
2025-03-03 13:41:57 -0500

    Añadiendo rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DepartmentComponent } from './components/department/department.component';
+import { CourseComponent } from './components/courses/courses.component';
+import { StudentComponent } from './components/student/student.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'departments', component: DepartmentComponent },
+  { path: 'courses', component: CourseComponent },
+  { path: 'students', component: StudentComponent },
+];
