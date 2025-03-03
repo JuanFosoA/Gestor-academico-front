c05d4c7b0458179e8e5a7700bc23ecd3bf25c08f
marcelagiraldo <lmarceag@gmail.com>
2025-03-03 14:44:17 -0500

    antes de merge

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        loadComponent:()=>import('./components/teacher-list/teacher-list.component').then((m)=>m.TeacherListComponent)
+    },
+    {
+        path:'teacher',
+        loadComponent:()=>import('./components/teacher-form/teacher-form.component').then((m)=>m.TeacherFormComponent)
+    },
+];
+
+@NgModule({
+    imports:[RouterModule.forRoot(routes)],
+    exports:[RouterModule]
+})
+
+export class AppRoutingModule{}
--- src/app/services/teachers.service.ts
+++ src/app/services/teachers.service.ts
@@ -1,38 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-
-export interface Teacher {
-  documento: string;
-  nombre: string;
-  fechaContrato:string
-}
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeachersService {
-  private apiUrl = 'http://localhost:3000/teachers'; // URL de tu backend
+ constructor(private http:HttpClient){}
 
-  constructor(private http: HttpClient) {}
+ getTeachers(){
 
-  getTeachers(): Observable<Teacher[]> {
-    return this.http.get<Teacher[]>(this.apiUrl);
-  }
+ }
 
-  getTeacher(documento: string): Observable<Teacher> {
-    return this.http.get<Teacher>(`${this.apiUrl}/${documento}`);
-  }
+ getTeacher(documento){
 
-  createTeacher(teacher: Teacher): Observable<Teacher> {
-    return this.http.post<Teacher>(this.apiUrl, teacher);
-  }
+ }
+ 
+ createTeacher(teacher){
 
-  updateTeacher(documento: string, teacher: Partial<Teacher>): Observable<any> {
-    return this.http.put(`${this.apiUrl}/${documento}`, teacher);
-  }
+ }
 
-  deleteTeacher(documento: string): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/${documento}`);
-  }
+ deleteTeacher(documento){
+
+ }
+
+ updateProduct(documento){
+
+ }
+
+
+ 
 }
